93ad6a8bb1e9a38808d42b4b5543371e556c836c
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-30 00:19:58 +0600

    use add All method

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,11 +7,16 @@
 //   var student = {"name": "aftab", "age": 20};
 //   print(student);
 // }
+// void main() {
+//   var student = new Map();
+//   student["name"] = "tom";
+//   student["age"] = 20;
+//   student["dept"] = "itm";
+//   student["major"] = "fintech";
+//   print(student);
+// }
 void main() {
-  var student = new Map();
-  student["name"] = "tom";
-  student["age"] = 20;
-  student["dept"] = "itm";
-  student["major"] = "fintech";
+  Map student = {"name": "Mahi", "age": 23};
+  student.addAll({"dept": "itm", "homeTown": "chandpur"});
   print(student);
 }
