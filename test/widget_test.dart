96ce02294ca4e7374d1f474319ce6143d0906fd4
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-30 12:04:09 +0600

    practice if else  statement

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,8 +15,58 @@
 //   student["major"] = "fintech";
 //   print(student);
 // }
+// void main() {
+//   Map student = {"name": "Mahi", "age": 23};
+//   student.addAll({"dept": "itm", "homeTown": "chandpur"});
+//   print(student);
+// }
+// void main() {
+//   Map student = {"name": "Mahi", "age": 23};
+//   student.clear();
+//   print(student);
+// }
+// void main() {
+//   Map student = {"name": "Mahi", "age": 23};
+//   student.remove("age");
+//   print(student);
+// }
+// void main() {
+//   var n = 10;
+//   if (n <= 20) {
+//     print("zero");
+//   } else {
+//     print("hero");
+//   }
+// }
+// void main() {
+//   var x = 10;
+//   var y = 20;
+//   if (x < y) {
+//     print("y is big");
+//   } else {
+//     print("x is big");
+//   }
+// }
 void main() {
-  Map student = {"name": "Mahi", "age": 23};
-  student.addAll({"dept": "itm", "homeTown": "chandpur"});
-  print(student);
+  var marks = 4;
+  if (marks > 90) {
+    print("A+");
+  }else if(marks > 80){
+    print("A");
+  }else if(marks > 70){
+        print("B");
+
+  }else if(marks > 60){
+        print("C");
+
+  }else if(marks > 50){
+        print("D");
+
+  }else if(marks > 40){
+        print("D-");
+
+  }else{
+        print("F");
+
+  }
 }
